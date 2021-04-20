662409ea09439a34f1e4b0d4dbf12931649ac0d7
champyrti <champi@gurke.club>
2021-04-20 20:30:26 +0200

    Allow control messages to be debugged

--- src/data.rs
+++ src/data.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use wvr_data::config::project_config::{FilterMode, InputConfig, RenderStageConfig, SampledInput};
 use wvr_data::DataHolder;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum ProviderInfo {
     Video((String, String, usize, usize, u32)),
     Picture((String, String, usize, usize)),
@@ -12,7 +12,7 @@ pub enum ProviderInfo {
     Cam((String, String, usize, usize)),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum SetInfo {
     Bpm(f64),
     Width(usize),
@@ -25,7 +25,7 @@ pub enum SetInfo {
     LockedSpeed(bool),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum RenderStageUpdate {
     Filter(String),
     FilterModeParams(FilterMode),
@@ -34,7 +34,7 @@ pub enum RenderStageUpdate {
     Name(String),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Message {
     Insert((String, InputConfig)),
     Set(SetInfo),
